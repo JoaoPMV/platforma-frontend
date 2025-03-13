ca13ced30d0ed47f4096816691cff54af66702cf
JoaoPMV <joaop.trilha@gmail.com>
2025-03-13 00:59:07 -0300

    Primeiro commit do backend

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3003";
+const API_URL = import.meta.env.VITE_API_URL; // Corrigido para usar o import.meta.env no Vite
 
 // Função para login do usuário
 export async function loginUser(userData) {
