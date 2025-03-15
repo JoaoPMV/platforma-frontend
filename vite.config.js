4ad15daa11df1338924c6ff2223d498de38d0332
JoaoPMV <joaop.trilha@gmail.com>
2025-03-14 22:42:51 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Defina a base para caminhos relativos à raiz
+  base: "/", // Defina a base para caminhos relativos à raiz
 });
