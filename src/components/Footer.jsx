83244a3fc00c86d488cc67a6caae9e4566d5dc7e
JoaoPMV <joaop.trilha@gmail.com>
2025-03-18 19:49:07 -0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="">
+    <footer className="container-footer">
       <div className="footer">
         <p className="text-light text-cente py-2 text-center">
           <FaWhatsapp className="react-icons-footer" />
