40b328e405c0a197edaab08d57b615ab1ea5bb5d
JoaoPMV <joaop.trilha@gmail.com>
2025-03-20 02:59:37 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Delete from "./pages/Delete";
+import Delete from "./pages/Delete.jsx";
 import Home from "./pages/Home";
-import QuemSomos from "./pages/QuemSomos";
+import QuemSomos from "./pages/QuemSomos.jsx";
 import Platform from "./pages/Platform";
-import Music from "./pages/Songs/Music";
-import BonJovi from "./pages/Songs/BonJovi";
-import EdSheeran from "./pages/Songs/EdSheeran";
-import SevenNation from "./pages/Songs/SevenNation";
-import BrunoMars from "./pages/Songs/BrunoMars";
-import JohnLennon from "./pages/Songs/JohnLennon";
-import JohnLegend from "./pages/Songs/JohnLegend";
-import Video from "./pages/Videos/Pratical2";
-import Model from "./pages/Songs/Model";
-
-import PrivateRoute from "./components/PrivateRoute"; // Certifique-se de que o caminho está correto
+import Music from "./pages/Songs/Music.jsx";
+import BonJovi from "./pages/Songs/BonJovi.jsx";
+import EdSheeran from "./pages/Songs/edSheeran.jsx";
+import SevenNation from "./pages/Songs/SevenNation.jsx";
+import BrunoMars from "./pages/Songs/BrunoMars.jsx";
+import JohnLennon from "./pages/Songs/JohnLennon.jsx";
+import JohnLegend from "./pages/Songs/JohnLegend.jsx";
+import Video from "./pages/Videos/Pratical2.jsx";
+import Model from "./pages/Songs/Model.jsx";
 
 function App() {
   return (
@@ -27,88 +25,16 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/deletar" element={<Delete />} />
-
-        {/* Rota para /platform, que agora é protegida */}
-        <Route
-          path="/platform"
-          element={
-            <PrivateRoute>
-              <Platform />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/music"
-          element={
-            <PrivateRoute>
-              <Music />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/music/bonjovi"
-          element={
-            <PrivateRoute>
-              <BonJovi />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/music/model"
-          element={
-            <PrivateRoute>
-              <Model />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/music/edsheeran"
-          element={
-            <PrivateRoute>
-              <EdSheeran />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/music/sevennation"
-          element={
-            <PrivateRoute>
-              <SevenNation />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/music/brunomars"
-          element={
-            <PrivateRoute>
-              <BrunoMars />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/music/johnlennon"
-          element={
-            <PrivateRoute>
-              <JohnLennon />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/music/johnlegend"
-          element={
-            <PrivateRoute>
-              <JohnLegend />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/platform/video"
-          element={
-            <PrivateRoute>
-              <Video />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/platform" element={<Platform />} />
+        <Route path="/platform/music" element={<Music />} />
+        <Route path="/platform/music/bonjovi" element={<BonJovi />} />
+        <Route path="/platform/music/model" element={<Model />} />
+        <Route path="/platform/music/edsheeran" element={<EdSheeran />} />
+        <Route path="/platform/music/sevennation" element={<SevenNation />} />
+        <Route path="/platform/music/brunomars" element={<BrunoMars />} />
+        <Route path="/platform/music/johnlennon" element={<JohnLennon />} />
+        <Route path="/platform/music/johnlegend" element={<JohnLegend />} />
+        <Route path="/platform/video" element={<Video />} />
       </Routes>
     </BrowserRouter>
   );
